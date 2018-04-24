82d11aef6dfc7ab30cc52f472520092a5cd2225b
Tuong Van Nguyen <tvan262@yahoo.com>
2018-04-24 18:40:14 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,6 @@
 # Write your code here.
 def badge_maker(name)
   name = [ "Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-  puts "Hello, my name is #{"name"}."
+  puts "Hello, my name is #{name}."
 end
 badge_maker("Ada")
